ed1f133ae479112f10bad95ef3c828abb7e8b57a
徐青山 <1370572852@qq.com>
2024-09-11 16:16:00 +0800

    1

--- rt/internal/utils/utils.go
+++ rt/internal/utils/utils.go
@@ -20,7 +20,7 @@ func ToSnakeCase(str string) string {
 	var result []rune
 	for i, r := range str {
 		if unicode.IsUpper(r) {
-			if i > 0 && (!unicode.IsUpper(r) || (i+1 < len(str) && unicode.IsLower(rune(str[i+1])))) {
+			if i > 0 && unicode.IsLower(rune(str[i-1])) {
 				result = append(result, '_')
 			}
 			result = append(result, unicode.ToLower(r))
