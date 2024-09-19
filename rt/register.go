93c48c5aff73bc9da43b1a76760145191a851ced
徐青山 <1370572852@qq.com>
2024-09-19 11:31:11 +0800

    1

--- rt/register.go
+++ rt/register.go
@@ -35,16 +35,16 @@ func registerRouter(pGroupRouter *gin.RouterGroup, regRouter *Router) {
 		name = regRouter.Path
 	}
 
-	// 检查路由组、路由结构体和方法是否有效，如果无效则记录日志并跳过路由注册。
-	if pGroupRouter == nil || regRouter == nil || regRouter.Method == "" {
-		log.Printf("%s: 没有 Method, 已跳过路由注册", name)
-		return
-	}
-
 	if regRouter.MODEL != nil && !regRouter.NoAutoMigrate {
 		if err := cf.ORMDB.AutoMigrate(regRouter.MODEL); err != nil {
 			log.Fatalf("%s: 数据库自动迁移失败: %v", name, err)
 		}
+	}
+
+	// 检查路由组、路由结构体和方法是否有效，如果无效则记录日志并跳过路由注册。
+	if pGroupRouter == nil || regRouter == nil || regRouter.Method == "" {
+		log.Printf("%s: 没有 Method, 已跳过路由注册", name)
+		return
 	}
 
 	pGroupRouter.Handle(
