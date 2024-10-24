eb06ffe2be3bd89de63dbcf0482bdc21506b820e
徐青山 <1370572852@qq.com>
2024-10-24 09:51:55 +0800

    1

--- bm/model.go
+++ bm/model.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Model struct {
-	ID        int            `gorm:"primarykey" json:"id"`
+	ID        int            `gorm:"primarykey;type:integer(11)" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"delete_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
